f865e98d853cda7160d066208ae86e93b7ae5444
xiaodu <you@example.com>
2020-06-18 15:52:39 +0800

    表单验证

--- assets/js/登录.js
+++ assets/js/登录.js
@@ -1,7 +1,23 @@
 //实现登录功能
 $(function() {
+    // 表单验证 
+    //基于layui自定义验证规则
+    var form = layui.form
+    form.verify({
+        uname:[/^[\S]{6,8}$/,'密码必须6到8位，且不能出现空格'],
+        pwd:function(value,item) {
+            // value表示，输入的值
+            // item表示，dom对象
+            // 正则 密码必须是六位数字
+            var reg = /^\d{6}$/
+            if(!reg.test(value)) {
+                return '密码必须是六位数字'
+            }
+        }
+    })
+
     //提交事件
-    $('form').submit(function(e) {
+    $('.layui-form').submit(function(e) {
         e.preventDefault()
         //获取表单数据
         var formData = $(this).serialize()
