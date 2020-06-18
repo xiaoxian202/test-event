52b987da725b9f1455eb313a490a22ff8d1108f1
xiaodu <you@example.com>
2020-06-18 17:51:40 +0800

    获取用户信息

--- assets/js/主页.js
+++ assets/js/主页.js
@@ -5,4 +5,41 @@ $(function() {
         //如果不存在，调转到登录页面
         location.href = './练习登录.html'
     }
+
+    //获取用户信息
+    function loadUserInFo() {
+        $.ajax({
+            type:'get',
+            url:'http://ajax.frontend.itheima.net/my/userinfo',
+            headers:{
+                Authorization:localStorage.getItem('mytoken')
+            },
+            success:function(res) {
+                if(res.status === 0) {
+                    //获取数据，把对应数据放到页面指定位置
+                    var data = res.data
+                    $('.welcome-uname').html(data.username)
+                    $('.log-uname').html(data.username)
+    
+                    //填充头像
+                    // info.user_pic = 'http://t.cn/RCzsdCq'
+                    if(data.user_pic) {
+                        //删除div，添加img
+                        $('.log-uname').siblings('div').remove()
+                        $('.log-uname').parent().prevend('<img src="'+data.user_pic+'">')
+    
+                        $('.welcome-uname').parent().siblings('div').remove()
+                        $('.welcome-uname').parent().parent().prevend('<img src="'+data.user_pic+'">')
+                        
+                    }else {
+                        //显示div
+                    }
+                }
+            }
+        })
+    }
+    loadUserInFo()
+
+    //退出功能
+    
 })
