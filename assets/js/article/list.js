bc16d654d6ddd88546d9087cb9745a4ae84c4a77
xiaodu <you@example.com>
2020-06-22 22:54:45 +0800

    文章列表

---
+++ assets/js/article/list.js
@@ -0,0 +1,82 @@
+$(function() {
+    //获取表单
+    var form = layui.form
+    // 页码值
+    var pagenum = 1
+    // 每页显示多少条数据
+    var pagesize = 10
+    // 处理日期的格式化： 基于模板引擎的过滤器
+    template.defaults.imports.formDate = function (data) {
+        // 实现日期的格式化：把参数data日期字符串转换为日期对象
+        var d = new Date(data)
+        var year = d.getFullYear()
+        var month = addZero(d.getMonth() + 1)
+        var day = addZero(d.getDate())
+        var hour = addZero(d.getHours())
+        var minutes = addZero(d.getMinutes())
+        var seconds = addZero(d.getSeconds())
+        return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':' + seconds
+    }
+
+    // 补零函数
+    function addZero(n) {
+        return n < 10 ? '0' + n : n;
+    }
+
+    //调用接口 分类下拉列表
+    function loadCateData() {
+        $.ajax({
+            type:'get',
+            url:'my/article/cates',
+            success:function(res) {
+                if(res.status === 0) {
+                    //依据模板引擎，渲染页面
+                    var tags = template('tpl-category',res)
+                    $('#category').html(tags)
+                    //更新渲染
+                    form.render('select')
+                }
+            }
+        })
+    }
+    loadCateData()
+
+    // 获取文章列表数据
+    function loadListData(param) {
+        $.ajax({
+            type:'get',
+            url:'my/article/list',
+            data:param,
+            success:function(res) {
+                console.log(res);
+                if(res.status === 0) {
+                    var tags = template('list-tpl',res)
+                    $('.layui-table tbody').html(tags)
+                }
+            }
+        })
+    }
+    loadListData({
+        pagenum:1,
+        pagesize:10
+    })
+
+    //搜索功能
+    $('#search-form').submit(function(e) {
+        e.preventDefault()
+        //获取表单数据
+        var fd = $(this).serializeArray()
+        console.log(fd);
+        var params = {
+            pagenum:pagenum,
+            pagesize:pagesize
+        }
+        fd.forEach(function(item) {
+            //动态添加属性
+            params[item.name] = item.value
+        })
+
+        loadListData(params)
+       
+    })
+})
